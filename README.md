29528ac4071fe44abe80120199394cffc728b9fc
rockwoodc <rockwoodce@gmail.com>
2022-09-14 19:56:39 -0400

    ss added to readme

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ This application can be used by small business owners to write and save notes.
 This app will help to organize thoughts and keep track of tasks that need to be completed.
 
 
-![Application](screen shot)
+![Application](./Screen%20Shot%202022-09-14%20at%207.54.31%20PM.png)
 
-[Link to deployed application](heroku link?)
+[Link to deployed application](heroku link)
--- server.js
+++ server.js
@@ -26,28 +26,70 @@ function createNewNote(body, noteArray) {
     return note;
 };
 
-
 //post requests represent the action of a client requesting the server to accept data
 app.post('/api/note', (req, res) => {
-    //set id based on what the next index of the array will be
-    req.body.id = notes.length.toString();
-    // if any data in req.body is incorrect, send 400 error back
-    if (!validatenote(req.body)) {
-        res.status(400).send('The note is not properly formatted.');
-    } else {
-    // add note to json file and note array in this function
-     const note = createNewnote(req.body, notes);
+  //set id based on what the next index of the array will be
+  req.body.id = notes.length.toString();
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validatenote(req.body)) {
+      res.status(400).send('The note is not properly formatted.');
+  } else {
+  // add note to json file and note array in this function
+   const note = createNewnote(req.body, notes);
 
-    res.json(note);
-    }
+  res.json(note);
+  }
 });
 
+app.get('/api/notes', (req, res) => {
+// get all notes in here 
+//figure out a way use json or the id, the title and the text,
+//luse local storage to make it persistant,  
+//if it goes we need to explain that information
+// if error we need to catch that error  
+})
+
+app.get('/notes/:id', (req,res) => {
+  const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+        //if the parameters don't exist a 404 message will appear
+      } else {
+        res.send(404);
+      }
+  // get all notes in here 
+//figure out a way use json or the id, the title and the text,
+//luse local storage to make it persistant,  
+//if it goes we need to explain that information
+// if error we need to catch that error 
+})
+
+app.get('/api/notes/:id', (req,res) => {
+  const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+        //if the parameters don't exist a 404 message will appear
+      } else {
+        res.send(404);
+      }
+})
+
+
+
 //route to sever.js
-app.get('/', (req, res) => {
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
   });
 
+// //route to a single note
+// app.get('/note', (req, res) => {
+//   res.sendFile(path.join(__dirname, './public/note.html'));
+// });
 
+//if user requests a route that doesn't exist they will be redirected to the homepage--THIS KIND OF ROUTE SHOLD ALWAYS BE LAST IN RELATION TO OTHER ROUTES
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
